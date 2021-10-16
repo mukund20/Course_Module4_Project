837565d7c8e8468f9919bb8823acc74c19b8de95
mukund20 <mukundatolay20@gmail.com>
2021-10-17 02:06:16 +0530

    Terraform

---
+++ terraform/alb.tf
@@ -0,0 +1,80 @@
+module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "5.16.0"
+
+  name = "project-alb"
+  load_balancer_type = "application"
+  vpc_id = module.vpc.vpc_id
+  subnets = [
+    module.vpc.public_subnets[0],
+    module.vpc.public_subnets[1]
+  ]
+  security_groups = [module.loadbalancer_sg.this_security_group_id]
+  # Listeners
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0 
+    }
+  ]  
+
+  target_groups = [
+    {
+      name_prefix          = "app-tg"
+      backend_protocol     = "HTTP"
+      backend_port         = 8080
+      target_type          = "instance"
+      deregistration_delay = 10
+      health_check = {
+
+        enabled             = true
+        interval            = 30
+        path                = "/app/*"
+        port                = "traffic-port"
+        healthy_threshold   = 3
+        unhealthy_threshold = 3
+        timeout             = 6
+        protocol            = "HTTP"
+        matcher             = "200-399"
+      }
+      protocol_version = "HTTP1"
+      # jenkins Target Group - Targets
+      targets = {
+        app-tg = {
+          target_id = module.ec2_private.id[0]
+          port      = 8080
+        }
+      }
+    }, 
+
+    {
+      name_prefix          = "j-tg"
+      backend_protocol     = "HTTP"
+      backend_port         = 8080
+      target_type          = "instance"
+      deregistration_delay = 10
+      health_check = {
+      
+        enabled             = true
+        interval            = 30
+        path                = "/jenkins/*"
+        port                = "traffic-port"
+        healthy_threshold   = 3
+        unhealthy_threshold = 3
+        timeout             = 6
+        protocol            = "HTTP"
+        matcher             = "200-399"
+      }
+      protocol_version = "HTTP1"
+      # jenkins Target Group - Targets
+      targets = {
+        j-tg = {
+          target_id = module.ec2_jenkins.id[0]
+          port      = 8080
+        },
+      }
+    }  
+  ]
+
+}
